6e55303bbecdf13f8b5c87a71bbaff71bbf39ee7
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-07-05 07:32:20 +0530

    Form

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -2,4 +2,29 @@ class GroupController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create 
+    @group = Group.new(
+      user_id: current_user.id,
+      name: group_params[:name]
+    )
+    
+    respond_to do |format|
+      if @group.save 
+        format.html { redirect_to group_index_path, notice: 'Group was successfully created.'}
+      else
+        format.html { render :new, status: :unprocessable_entity, alert: "Something went wrong" }
+      end
+    end
+  end
+
+  private 
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
